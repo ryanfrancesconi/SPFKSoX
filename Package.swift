0d0242d639f7e51a5e01a662defb2318a92ea02c
Ryan Francesconi <thiscomputermagic@gmail.com>
2025-11-24 17:17:10 -0800

    update Package.swift template to use useLocalDependencies as a switch between local and released (remote)

--- Package.swift
+++ Package.swift
@@ -2,108 +2,142 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
+import Foundation
 
-// This package will assume C / Objective-C interoperability as it's more common.
-// C++ could be enabled with:
-// swiftSettings: [.interoperabilityMode(.Cxx)]
-
-// Swift target
-private let name: String = "SPFKSoX"
-
-// C/C++ target
-private let nameC: String = "\(name)C"
-
+private let name: String = "SPFKSoX" // Swift target
+private let dependencyNames: [String] = ["SPFKAudioBase", "SPFKTesting"]
+private let dependencyNamesC: [String] = []
+private let dependencyBranch = "main"
+private let useLocalDependencies: Bool = false
 private let platforms: [PackageDescription.SupportedPlatform]? = [
-    .macOS(.v12)
+    .macOS(.v12),
 ]
+
+let remoteDependencies: [RemoteDependency] = []
+
+// Special case for local binary targets
+let binaryTargetNames = ["libsndfile", "libsamplerate", "libsox", "libmad", "libmp3lame", "libmpg123"]
+
+private var cTargetDependencies: [PackageDescription.Target.Dependency] {
+    binaryTargetNames.map {
+        .target(name: $0)
+    }
+}
+
+private let binaryTargets: [PackageDescription.Target] =
+    binaryTargetNames.map {
+        PackageDescription.Target.binaryTarget(
+            name: $0,
+            path: "Frameworks/\($0).xcframework"
+        )
+    }
+
+
+
+// MARK: - Reusable Code for a Swift + C package
+
+struct RemoteDependency {
+    let package: PackageDescription.Package.Dependency
+    let product: PackageDescription.Target.Dependency
+}
+
+private let nameC: String = "\(name)C" // C/C++ target
+private let nameTests: String = "\(name)Tests" // Test target
+private let githubBase = "https://github.com/ryanfrancesconi"
 
 private let products: [PackageDescription.Product] = [
-    .library(
-        name: name,
-        targets: [name, nameC]
-    )
+    .library(name: name, targets: [name, nameC])
 ]
 
-private let dependencies: [PackageDescription.Package.Dependency] = [
-    .package(name: "SPFKAudioBase", path: "../SPFKAudioBase"),
-    .package(name: "SPFKTesting", path: "../SPFKTesting"),
+private var packageDependencies: [PackageDescription.Package.Dependency] {
+     let local: [PackageDescription.Package.Dependency] =
+        dependencyNames.map {
+            .package(name: "\($0)", path: "../\($0)") // assumes the package garden is in one folder
+        }
 
-//     .package(url: "https://github.com/ryanfrancesconi/SPFKTesting", branch: "main"),
-]
+        
+     let remote: [PackageDescription.Package.Dependency] =
+        dependencyNames.map {
+            .package(url: "\(githubBase)/\($0)", branch: dependencyBranch)
+        }
+    
+    var value = useLocalDependencies ? local : remote
+    
+    if !remoteDependencies.isEmpty {
+        value.append(contentsOf: remoteDependencies.map { $0.package } )
+    }
+    
+    return value
+}
 
+// is there a Sources/[NAME]/Resources folder?
+private var swiftTargetResources: [PackageDescription.Resource]? {
+    // package folder
+    let root = URL(fileURLWithPath: #file).deletingLastPathComponent()
+    
+    let dir = root.appending(component: "Sources")
+        .appending(component: name)
+        .appending(component: "Resources")
+    
+    let exists = FileManager.default.fileExists(atPath: dir.path)
+    
+    return exists ? [.process("Resources")] : nil
+}
+
+private var swiftTargetDependencies: [PackageDescription.Target.Dependency] {
+    let names = dependencyNames.filter { $0 != "SPFKTesting" }
+    
+    var value: [PackageDescription.Target.Dependency] = names.map {
+        .byNameItem(name: "\($0)", condition: nil)
+    }
+    
+    value.append(.target(name: nameC))
+    
+    if !remoteDependencies.isEmpty {
+        value.append(contentsOf: remoteDependencies.map { $0.product } )
+    }
+    
+    return value
+}
+
+private let swiftTarget: PackageDescription.Target = .target(
+    name: name,
+    dependencies: swiftTargetDependencies,
+    resources: swiftTargetResources
+)
+
+private var testTargetDependencies: [PackageDescription.Target.Dependency] {
+    var array: [PackageDescription.Target.Dependency] = [
+        .byNameItem(name: name, condition: nil),
+        .byNameItem(name: nameC, condition: nil),
+    ]
+
+    if dependencyNames.contains("SPFKTesting") {
+        array.append(.byNameItem(name: "SPFKTesting", condition: nil))
+    }
+    
+    return array
+}
+
+private let testTarget: PackageDescription.Target = .testTarget(
+    name: nameTests,
+    dependencies: testTargetDependencies
+)
+
+private let cTarget: PackageDescription.Target = .target(
+    name: nameC,
+    dependencies: cTargetDependencies,
+    publicHeadersPath: "include",
+    cSettings: [
+        .headerSearchPath("include_private")
+    ],
+    cxxSettings: [
+        .headerSearchPath("include_private")
+    ]
+)
 
 private let targets: [PackageDescription.Target] = [
-    // Swift
-    .target(
-        name: name,
-        dependencies: [
-            .target(name: nameC),
-            .byNameItem(name: "SPFKAudioBase", condition: nil)
-        ]
-    ),
-    
-    // C
-    .target(
-        name: nameC,
-        dependencies: [
-            .target(name: "libsndfile"),
-            .target(name: "libsamplerate"),
-            .target(name: "libsox"),
-            .target(name: "libmad"),
-            .target(name: "libmp3lame"),
-            .target(name: "libmpg123"),
-        ],
-        publicHeadersPath: "include",
-        cSettings: [
-            .headerSearchPath("include_private")
-        ],
-        cxxSettings: [
-            .headerSearchPath("include_private")
-        ]
-    ),
-    
-    // path: relative to the package root
-
-    .binaryTarget(
-        name: "libsndfile",
-        path: "Frameworks/libsndfile.xcframework"
-    ),
-    
-    .binaryTarget(
-        name: "libsamplerate",
-        path: "Frameworks/libsamplerate.xcframework"
-    ),
-    
-    .binaryTarget(
-        name: "libsox",
-        path: "Frameworks/libsox.xcframework"
-    ),
-
-    .binaryTarget(
-        name: "libmad",
-        path: "Frameworks/libmad.xcframework"
-    ),
-
-    .binaryTarget(
-        name: "libmp3lame",
-        path: "Frameworks/libmp3lame.xcframework"
-    ),
-
-    .binaryTarget(
-        name: "libmpg123",
-        path: "Frameworks/libmpg123.xcframework"
-    ),
-
-    .testTarget(
-        name: "\(name)Tests",
-        dependencies: [
-            .byNameItem(name: name, condition: nil),
-            .byNameItem(name: nameC, condition: nil),
-            .byNameItem(name: "SPFKTesting", condition: nil)
-        ],
-        resources: [
-        ]
-    )
+    swiftTarget, cTarget, testTarget
 ]
 
 let package = Package(
@@ -111,7 +145,7 @@ let package = Package(
     defaultLocalization: "en",
     platforms: platforms,
     products: products,
-    dependencies: dependencies,
-    targets: targets,
+    dependencies: packageDependencies,
+    targets: targets + binaryTargets,
     cxxLanguageStandard: .cxx20
 )
